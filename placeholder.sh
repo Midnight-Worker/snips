d56942d00ea6e9860c5346da989316292e4cb6e7
Maik Tappe <maik.tappe82@googlemail.com>
2026-07-26 23:56:18 +0200

    placeholder

--- placeholder.sh
+++ placeholder.sh
@@ -1,10 +1,25 @@
 ph() {
-    local url="https://placehold.co/${1}.png"
+    local size="$1"
+    local filename="$2"
+    local text="$3"
+    local background="${4#\#}"
+    local textcolor="${5#\#}"
+    local url="https://placehold.co/${size}"
 
-    if [ -n "$3" ]; then
-        local text="${3// /+}"
-        url="${url}?text=${text}"
+    if [[ -n "$background" || -n "$textcolor" ]]; then
+        if [[ -z "$background" || -z "$textcolor" ]]; then
+            echo "Fehler: Hintergrund- und Textfarbe müssen angegeben werden."
+            return 1
+        fi
+
+        url+="/${background}/${textcolor}"
     fi
 
-    curl -L "$url" -o "$2"
+    url+=".png"
+
+    if [[ -n "$text" ]]; then
+        curl -L -G --data-urlencode "text=$text" "$url" -o "$filename"
+    else
+        curl -L "$url" -o "$filename"
+    fi
 }
